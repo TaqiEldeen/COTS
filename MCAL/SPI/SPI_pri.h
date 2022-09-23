6af715f1f400a1fae99571cb5c5d744143fdcf59
TaqiEldeen <takeycoo@gmail.com>
2022-09-23 22:04:33 +0200

    SPI private added

--- MCAL/SPI/SPI_pri.h
+++ MCAL/SPI/SPI_pri.h
@@ -10,5 +10,40 @@
 #define MCAL_SPI_SPI_PRI_H_
 
 
+/************** 	INT status	  **************/
+#define INT_ENABLE		1
+#define INT_DISABLE		0
+
+
+/************** 	Data order	  **************/
+#define LSB_FIRST	1
+#define MSB_FIRST	0
+
+
+/************** 	Master/Slave Select	  **************/
+#define SPI_MASTER		1
+#define SPI_SLAVE		0
+
+
+/************** 	Clock Polarity	  **************/
+#define IDLE_SCK_RISING		1
+#define IDLE_SCK_FALLING	0
+
+
+/************** 	Clock Phase	  **************/
+#define SETUP_TRAILING		0
+#define SETUP_LEADING		1
+
+
+/************** 	clock rate select	  **************/
+#define PRESCALE_4		0
+#define PRESCALE_16		1
+#define PRESCALE_64		2
+#define PRESCALE_128	3
+#define PRESCALE_2		4
+#define PRESCALE_8		5
+#define PRESCALE_32		6
+#define PRESCALE_64		7
+
 
 #endif /* MCAL_SPI_SPI_PRI_H_ */
